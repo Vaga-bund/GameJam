4bdbce9d617cf12d50b40e701504f154767c1dd0
Vaga-bund <wlwogh0621@naver.com>
2022-08-20 20:55:59 +0900

    repair

--- Assets/Scripts/Arkanoid/Bullets.cs
+++ Assets/Scripts/Arkanoid/Bullets.cs
@@ -36,17 +36,17 @@ public class Bullets : MonoBehaviour
 
         rb.velocity = direction * Mathf.Max(speed, shootSpeed);
 
-        // 공 버그나면 지울 것!
-        if (rb.velocity.magnitude != 0 && rb.velocity.x < 0.15f && rb.velocity.x > -0.15f)
-        {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(new Vector2(-0.4f, rb.velocity.y > 0 ? 1 : -1).normalized);
-        }
-        if (rb.velocity.magnitude != 0 && rb.velocity.y < 0.15f && rb.velocity.y > -0.15f)
-        {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(new Vector2(rb.velocity.x > 0 ? 1 : -1, 0.4f).normalized);
-        }
+        //// 공 버그나면 지울 것!
+        //if (rb.velocity.magnitude != 0 && rb.velocity.x < 0.15f && rb.velocity.x > -0.15f)
+        //{
+        //    rb.velocity = Vector2.zero;
+        //    rb.AddForce(new Vector2(-0.4f, rb.velocity.y > 0 ? 1 : -1).normalized);
+        //}
+        //if (rb.velocity.magnitude != 0 && rb.velocity.y < 0.15f && rb.velocity.y > -0.15f)
+        //{
+        //    rb.velocity = Vector2.zero;
+        //    rb.AddForce(new Vector2(rb.velocity.x > 0 ? 1 : -1, 0.4f).normalized);
+        //}
 
         Vector3 currentEuler = transform.rotation.eulerAngles;
         float rotation = currentEuler.z;
